b17b9a1ad3cf624ec71a330995060f7a4f8843ae
Rukia Sheikh-Mohamed <rsheikh-mohamed@mpr.org>
2018-08-27 14:56:25 -0500

    rendering pet API to DOM using state

--- src/App.js
+++ src/App.js
@@ -13,27 +13,63 @@ class App extends React.Component {
   constructor (props) {
     super(props);
 
-    this.stat = {
+    this.state = {
       pets: []
     };
   }
 
   componentDidMount(){
-    // i rendered to the DOM for the first time now I can do other things
-    // called once per Component
-    const promise = petfinder.breed.list({ animal: "dog"}) //returns a promise (object that returns future value)
-    promise.then(console.log, console.error)
+    petfinder.pet
+    .find({
+      output: "full",
+      location: "Minneapolis, MN"
+    })
+    .then(data => {
+      let pets;
+      if (data.petfinder.pets && data.petfinder.pets.pet){
+        if (Array.isArray(data.petfinder.pets.pet)){
+          pets = data.petfinder.pets.pet;
+        } else {
+          pets = [data.petfinder.pets.pet] //just 1 animal wrap in array
+        }
+      } else {
+        pets = [] //if you find nothing make pets an empty array
+      }
+      this.setState({
+        // pets: pets (same as below line)
+        pets
+      });
+    });
   }
   render() {
-// using JSX
     return (
       <div>
         <h1>Adapt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-        <Pet name="Doink" animal="cat" breed="Mix" />
-      </div>
+        <div>
+          {this.state.pets.map(pet => {
+
+            let breed;
+
+            if (Array.isArray(pet.breeds.breed)){
+              breed = pet.breeds.breed.join(", ")
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return(
+              <Pet
+                animal={pet.animal}
+                key={pet.id}
+                name={pet.name}
+                breed={breed}
+              />
+            )
+          })}
+      //   </div>
+      //   <Pet name="Luna" animal="dog" breed="Havanese" />
+      //   <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      //   <Pet name="Doink" animal="cat" breed="Mix" />
+      // </div>
     )
   }
 }
-render(<App/>, document.getElementById("root")); //render root to div on line 14
+render(<App/>, document.getElementById("root"));
