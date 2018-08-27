a9de30752f7c5be6002545d975611971ba7dda93
Rukia Sheikh-Mohamed <rsheikh-mohamed@mpr.org>
2018-08-27 14:24:46 -0500

    using componentDidMount with promise

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+
+const petfinder = pf({
+  key: process.env.API_Key,
+  secret: process.env.API_Secret
+})
+
 class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.stat = {
+      pets: []
+    };
+  }
+
+  componentDidMount(){
+    // i rendered to the DOM for the first time now I can do other things
+    // called once per Component
+    const promise = petfinder.breed.list({ animal: "dog"}) //returns a promise (object that returns future value)
+    promise.then(console.log, console.error)
+  }
   render() {
 // using JSX
     return (
