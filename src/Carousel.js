8a7a9f11b9ae80dedae50dc456e7376cc9738aee
Rukia Sheikh-Mohamed <rsheikh-mohamed@mpr.org>
2018-08-28 13:29:34 -0500

    add click event for interaction

--- src/Carousel.js
+++ src/Carousel.js
@@ -14,15 +14,24 @@ class Carousel extends React.Component {
 
     return { photos };
   }
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index //plus sign takes a string and makes it a number
+    });
+  };
   render() {
     const { photos, active } = this.state;
+
     return (
       <div className="carousel">
         <img src={photos[active].value} alt="animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+            /* eslint-disable-next-line */
             <img
+              onClick={this.handleIndexClick}
               key={photo.value}
+              data-index={index}
               src={photo.value}
               className={index === active ? "active" : ""}
               alt="animal thumnbail"
