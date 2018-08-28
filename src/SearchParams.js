4c3e59db8e4bb50f663794c6e615c9448fc37fd4
Rukia Sheikh-Mohamed <rsheikh-mohamed@mpr.org>
2018-08-28 14:59:42 -0500

    add breeds to select list

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,17 @@
 import React from "React";
-import { ANIMALS } from 'petfinder-client';
+import pf, { ANIMALS } from 'petfinder-client';
+
+const petfinder = pf({
+  key: process.env.API_Key,
+  secret: process.env.API_Secret
+})
 
 class SearchParams extends React.Component {
   state = {
     location: "Minneapolis, MN",
     animal: "",
-    breed: ""
+    breed: "",
+    breeds: []
   };
   handleLocationChange = event => {
     this.setState({
@@ -14,9 +20,38 @@ class SearchParams extends React.Component {
   };
   handleAnimalChange = event => {
     this.setState({
-      animal: event.target.value
+      animal: event.target.value,
+      breed: ""
+    },
+    this.getBreeds
+  );
+  };
+  handleBreedChange = event => {
+  this.setState({
+    breed: event.target.value
     });
   };
+  getBreeds(){
+    if(this.state.animal){
+      petfinder.breed
+      .list({ animal: this.state.animal })
+      .then(data => {
+        if (
+          data.petfinder &&
+          data.petfinder.breeds &&
+          Array.isArray(data.petfinder.breeds.breed)
+        ) {
+          this.setState({
+             breeds: data.petfinder.breeds.breed
+           });
+      } else {
+        this.setState({ breeds: [] });
+      }
+  });
+  } else {
+    this.setState({ breeds: [] });
+  }
+}
   render(){
     return (
       <div className="search-params">
@@ -45,6 +80,24 @@ class SearchParams extends React.Component {
           }
         </select>
       </label>
+      <label htmlFor="breed">
+        Breed
+        <select
+         id="breed"
+         value={this.state.breed}
+         onChange={this.handleBreedChange}
+         onBlur={this.handleBreedChange}
+         disabled={!this.state.breeds.length === 0}
+         >
+        <option />
+        {
+          this.state.breeds.map(breed => (
+          <option key={breed} value={breed}>{breed}</option>
+          ))
+        }
+        </select>
+      </label>
+      <button>Submit</button>
       </div>
     );
   }
